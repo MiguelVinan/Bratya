ce1125dd5d1859653ba3343a76698603e9c37c4f
luisantos <luisio231990@hotmail.com>
2016-01-27 17:51:19 -0500

    agregeList-cambiosIncluso-cambiosServicios

--- footer.php
+++ footer.php
@@ -75,6 +75,147 @@
   <div id="markWater-contact">
     <span class="markWater markWater-contact">Contactanos</span>
   </div>
+
+<section class="ministeriosEcuador row center-xs">
+  <div class="ministeriosEcuadorImagen col-xs-12">
+
+  </div>
+  <div class="ministeriosEcuadorData row col-sm-12">
+    <!-- BLOQUE 1 -->
+    <div class="row col-xs-12 col-sm-12">
+    <!-- Presidencia -->
+       <div class="entidad col-xs-12 col-sm-6">
+        <a href="#" class="btn-ministerios s-visible">
+          <h3><i class="fa fa-plus"></i> Presidencia</h3>
+        </a>
+        <div class="list is-hidden">
+          <ul class="list-ministerios">
+          <?php if( have_rows('presidencias') ):
+            while ( have_rows('presidencias') ) : the_row(); ?>
+            <li><a target="_blank" href="<?php the_sub_field('url_presidencia') ?>"><?php the_sub_field('name_presidencia') ?></a></li>
+            <?php endwhile;
+              else :
+            endif;
+            ?>
+          </ul>
+        </div>
+      </div>
+
+    <!-- Vicepresidencia -->
+      <div class="entidad col-xs-12 col-sm-6">
+        <a href="#" class="btn-ministerios s-visible">
+          <h3><i class="fa fa-plus"></i> Vicepresidencia</h3>
+        </a>
+        <div class="list is-hidden">
+          <ul class="list-ministerios " >
+            <?php if( have_rows('vicepresidencias') ):
+              while ( have_rows('vicepresidencias') ) : the_row(); ?>
+
+              <li><a target="_blank" href="<?php the_sub_field('url_vicepresidencia') ?>"><?php the_sub_field('nombre_vicepresidencia') ?></a></li>
+
+              <?php endwhile;
+                else :
+              endif;
+              ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- bloque 2 -->
+    <div class="row col-xs-12 col-sm-12">
+    <!-- Secretarías Nacionales -->
+      <div class="entidad col-xs-12 col-sm-6">
+        <a href="#" class="btn-ministerios s-visible">
+          <h3><i class="fa fa-plus"></i> Secretarías Nacionales</h3>
+        </a>
+        <div class="list is-hidden">
+          <ul class="list-ministerios">
+            <?php if( have_rows('secretaria_nacionales') ):
+              while ( have_rows('secretaria_nacionales') ) : the_row(); ?>
+
+              <li><a target="_blank" href="<?php the_sub_field('url_secretaria_nacional') ?>"><?php the_sub_field('name_secretaria_nacional') ?></a></li>
+
+              <?php endwhile;
+                else :
+              endif;
+              ?>
+          </ul>
+        </div>
+      </div>
+
+    <!-- Secretarías -->
+      <div class="entidad col-xs-12 col-sm-6">
+        <a href="#" class="btn-ministerios s-visible">
+          <h3><i class="fa fa-plus"></i> Secretarías</h3>
+        </a>
+        <div class="list is-hidden">
+          <ul class="list-ministerios">
+            <?php if( have_rows('secretarias') ):
+              while ( have_rows('secretarias') ) : the_row(); ?>
+
+              <li><a  target="_blank" href="<?php the_sub_field('url_secretarias') ?>"><?php the_sub_field('name_secretarias') ?></a></li>
+
+              <?php endwhile;
+                else :
+              endif;
+              ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- bloque 3 -->
+    <div class="col-xs-12 col-sm-6">
+    <!-- Ministerios Coordinadores -->
+      <div class="">
+        <div class="entidad">
+          <a href="#" class="btn-ministerios s-visible">
+            <h3><i class="fa fa-plus"></i> Ministerios Coordinadores</h3>
+          </a>
+          <div class="list is-hidden">
+            <ul class="list-ministerios">
+              <?php if( have_rows('ministerios_coordinadores') ):
+                while ( have_rows('ministerios_coordinadores') ) : the_row(); ?>
+
+                <li><a  target="_blank" href="<?php the_sub_field('url_ministerios_coordinadores') ?>"><?php the_sub_field('name_ministerios_coordinadores') ?></a></li>
+
+                <?php endwhile;
+                  else :
+                endif;
+                ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- BLOQUE 4 -->
+    <div class="col-xs-12 col-sm-6">
+      <div class="entidad">
+        <a href="#" class="btn-ministerios s-visible">
+          <h3><i class="fa fa-plus"></i> Ministerios</h3>
+        </a>
+        <div class="list is-hidden">
+          <ul class="list-ministerios">
+            <?php if( have_rows('ministerios') ):
+              while ( have_rows('ministerios') ) : the_row(); ?>
+
+              <li><a target="_blank" href="<?php the_sub_field('url_ministerios') ?>"><?php the_sub_field('name_ministerios') ?></a></li>
+
+              <?php endwhile;
+                else :
+              endif;
+              ?>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
   <div class="Copyright row between-sm">
     <span class="col-xs-12 col-sm-6 center-xs end-sm">Designed & Developed by <a href="//miguelvinan.com">@MiguelVinan</a></span>
     <span class="col-xs-12 col-sm-6 center-xs start-sm">2015 ©Bratya Azanza, todos los derechos reservados.</span>
